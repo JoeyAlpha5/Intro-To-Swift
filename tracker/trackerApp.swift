6544e57a6442bc0cacfe58747272db0af3cc5996
Jalome <chjalome@gmail.com>
2021-01-19 16:53:00 +0200

    Initial Commit

---
+++ tracker/trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  trackerApp.swift
+//  tracker
+//
+//  Created by Jalome Chirwa on 2021/01/19.
+//
+
+import SwiftUI
+
+@main
+struct trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
