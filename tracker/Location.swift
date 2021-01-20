be02a336d54ec2328e11932328d4a094136aa0ee
Jalome <chjalome@gmail.com>
2021-01-20 14:04:10 +0200

    first commit

---
+++ tracker/Location.swift
@@ -0,0 +1,8 @@
+//
+//  Location.swift
+//  tracker
+//
+//  Created by Jalome Chirwa on 2021/01/20.
+//
+
+import Foundation
---
+++ tracker/WorldView.swift
@@ -0,0 +1,20 @@
+//
+//  WorldView.swift
+//  tracker
+//
+//  Created by Jalome Chirwa on 2021/01/20.
+//
+
+import SwiftUI
+
+struct WorldView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WorldView_Previews: PreviewProvider {
+    static var previews: some View {
+        WorldView()
+    }
+}
